3af309bb0985f11feae93bab6a71e21a6cb6f4ef
Louis Le Bras <louislebras@macbook-pro-de-louis.home>
2023-01-25 16:02:30 +0100

    ngdngfd

---
+++ public/sources/js/classHover.js
@@ -0,0 +1,8 @@
+$(".container-hover-loulou").hover(
+  function () {
+    $(".container-image-hover-heading").addClass("hover-donloulou");
+  },
+  function () {
+    $(".container-image-hover-heading").removeClass("hover-donloulou");
+  }
+);
